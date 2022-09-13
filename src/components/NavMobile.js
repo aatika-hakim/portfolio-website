e7b14b3eeee4035bcbd55eaaa37b1d4f64b6f868
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-13 19:09:35 +0500

    importing component, motion, link

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,9 +1,20 @@
 import React from 'react';
+
 // import
 import { navigation } from '../data';
+
 // import icons
 import {XIcon} from '@heroicons/react/outline';
 import {MenuAlt3Icon} from '@heroicons/react/outline';
+
+// import component
+import Socials from './Socials';
+
+// import framer motion
+import motion from 'framer-motion';
+
+import Link from 'react-scroll';
+
 
 const NavMobile = () => {
   return (
