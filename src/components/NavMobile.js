145b5b64d9e0a33aae187e84c8cef4324f4c3db6
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-15 09:13:33 +0500

    nav mobile , menu button

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 // import
 import { navigation } from '../data';
@@ -11,14 +11,66 @@ import {MenuAlt3Icon} from '@heroicons/react/outline';
 import Socials from './Socials';
 
 // import framer motion
-import motion from 'framer-motion';
+import {motion} from 'framer-motion';
 
+// import link
 import Link from 'react-scroll';
 
 
 const NavMobile = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  // framer motion variants={circleVarients}
+  const circleVariants = {
+    hidden:{
+      scale: 0,
+    },
+
+    visible:{
+      scale: 180,
+
+      transition:{
+        type: 'spring',
+        stiffness: 160,
+        damping: 60,
+      },
+    },
+  }
+
+  const ulVariants = {
+    hidden:{
+      opacity: 0,
+    },
+
+    visible:{
+      opacity: 1,
+
+      transition:{
+        delay: 0.1,
+      },
+    },
+  }
+
   return (
-    <nav className=''></nav>
+    <nav className='relative' >
+      {/* Menu icons */}
+      <div onClick={()=> setIsOpen(true)}
+      className='cursor-pointer text-white'>
+
+        <MenuAlt3Icon className='w-8 h-8' />
+
+        {/* circle */}
+        <motion.div
+        variants={circleVariants}
+        initial='hidden'
+        animate={isOpen ? 'visible' : 'hidden'}
+        className='w-4 h-4 rounded-full bg-accent
+        fixed top-0 right-0'>
+
+        </motion.div>
+      </div>
+    </nav>
   )
 }
 
