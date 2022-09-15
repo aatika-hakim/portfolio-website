1069db628f9d4241cae96ae70a725bf166cdf103
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-15 20:23:29 +0500

    Projects completed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,65 +1,71 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 // import projects data
-import { projectsData } from '../data';
+import { projectsData } from '../data'
 
 // import projects nav data
-import { projectsNav } from '../data';
+import { projectsNav } from '../data'
 
 // import component
-import Project from './Project';
+import Project from './Project'
 
 const Projects = () => {
-  const[item, setItem] = useState({name: 'all'});
-  const[projects, setProjects]=useState([]);
-  const[active, setActive]=useState(0);
+  const [item, setItem] = useState({ name: 'all' })
+  const [projects, setProjects] = useState([])
+  const [active, setActive] = useState(0)
 
-  useEffect(()=>{
+  useEffect(() => {
     // get projects base on item
-    if(item.name === 'all'){
-      setProjects(projectsData);
-    }else{
-      const newProjects = projectsData.filter(
-        (project) =>{
-          return project.category.toLowerCase() === item.name;
-        });
-        setProjects(newProjects);
+    if (item.name === 'all') {
+      setProjects(projectsData)
+    } else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category.toLowerCase() === item.name
+      })
+      setProjects(newProjects)
     }
-  },[item]);
+  }, [item])
 
-  const handleClick = (e, index) =>{
-    setItem({name: e.target.textContent.toLowerCase()});
-    setActive(index);
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent.toLowerCase() })
+    setActive(index)
   }
 
   return (
     /* nav */
     <div>
-    <nav className='mb-12 max-w-xl mx-auto'>
-      <ul className='flex flex-col md:flex-row
-      justify-evenly items-center text-white'>
-
-      {projectsNav.map((item, index)=>{
-        return <li onClick={(e)=>{
-          handleClick(e,index)
-        }}
-        className={`${active === index ? 'active': ''} 
+      <nav className="mb-12 max-w-xl mx-auto">
+        <ul
+          className="flex flex-col md:flex-row
+      justify-evenly items-center text-white"
+        >
+          {projectsNav.map((item, index) => {
+            return (
+              <li
+                onClick={(e) => {
+                  handleClick(e, index)
+                }}
+                className={`${active === index ? 'active' : ''} 
         cursor-pointer capitalize m-4`}
-        key={index}>
-        {item.name}
-        </li>
-        
-      })}
-      </ul>
-    </nav>
-    {/* projects */}
-    <section>
-      {projects.map((item)=>{
-        return <Project item={item} key={item.id} />
-      })}
-    </section>
+                key={index}
+              >
+                {item.name}
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+      {/* projects */}
+      <section
+        className="grid lg:grid-cols-3 
+    gap-y-12 lg:gap-x-8 lg:gap-y-8"
+      >
+        {projects.map((item) => {
+          return <Project item={item} key={item.id} />
+        })}
+      </section>
     </div>
   )
 }
 
-export default Projects;
+export default Projects
