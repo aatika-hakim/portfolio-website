2b03c86a36ffba131715ca2397ee46be401254e6
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-15 13:39:52 +0500

    import skills component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Brands from './components/Brands';
 import About from './components/About';
+import Skills from './components/Skills';
 
 
 // import components
@@ -15,6 +16,7 @@ const App = () => {
       <Hero />
       <Brands />
       <About />
+      <Skills />
       <div style={{height: '2000px'}}></div>
     </>
   );
