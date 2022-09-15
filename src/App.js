c4d7fd55edd37dbf8e38e76eedd733c344dad9df
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-15 12:19:10 +0500

    import about component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Brands from './components/Brands';
+import About from './components/About';
 
 
 // import components
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <Hero />
       <Brands />
+      <About />
       <div style={{height: '2000px'}}></div>
     </>
   );
