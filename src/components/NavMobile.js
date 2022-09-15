77f9bd83ad2919a4c7e18b89f009232c25e14e55
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-15 10:59:18 +0500

    navigation data

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,36 +1,34 @@
-import React,{useState} from 'react';
-
-// import
-import { navigation } from '../data';
+import React, { useState } from 'react'
 
 // import icons
-import {XIcon} from '@heroicons/react/outline';
-import {MenuAlt3Icon} from '@heroicons/react/outline';
+import { XIcon } from '@heroicons/react/outline'
+import { MenuAlt3Icon } from '@heroicons/react/outline'
+
+// import navigation
+import { navigation } from '../data'
 
 // import component
-import Socials from './Socials';
+import Socials from './Socials'
 
 // import framer motion
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion'
 
 // import link
-import Link from 'react-scroll';
-
+import {Link} from 'react-scroll'
 
 const NavMobile = () => {
-
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
 
   // framer motion variants
   const circleVariants = {
-    hidden:{
+    hidden: {
       scale: 0,
     },
 
-    visible:{
+    visible: {
       scale: 180,
 
-      transition:{
+      transition: {
         type: 'spring',
         stiffness: 160,
         damping: 60,
@@ -39,56 +37,73 @@ const NavMobile = () => {
   }
 
   const ulVariants = {
-    hidden:{
+    hidden: {
       opacity: 0,
     },
 
-    visible:{
+    visible: {
       opacity: 1,
 
-      transition:{
+      transition: {
         delay: 0.1,
       },
     },
   }
 
   return (
-    <nav className='relative' >
+    <nav className="relative">
       {/* Menu icons */}
-      <div onClick={()=> setIsOpen(true)}
-      className='cursor-pointer text-white'>
-
-        <MenuAlt3Icon className='w-8 h-8' />
-
-</div>
-        {/* circle */}
-        <motion.div
+      <div
+        onClick={() => setIsOpen(true)}
+        className="cursor-pointer text-white"
+      >
+        <MenuAlt3Icon className="w-8 h-8" />
+      </div>
+      {/* circle */}
+      <motion.div
         variants={circleVariants}
-        initial='hidden'
+        initial="hidden"
         animate={isOpen ? 'visible' : 'hidden'}
-        className='w-4 h-4 rounded-full bg-accent
-        fixed top-0 right-0'>
-        </motion.div>
+        className="w-4 h-4 rounded-full bg-accent
+        fixed top-0 right-0"
+      ></motion.div>
 
-        <motion.ul 
+      <motion.ul
         variants={ulVariants}
-        initial='hiddden'
+        initial="hiddden"
         animate={isOpen ? 'visible' : 'hidden'}
-        className= {`${isOpen ? 'right-0' : 'right-full'} 
+        className={`${isOpen ? 'right-0' : 'right-full'} 
         fixed top-0 bottom-0 w-full flex flex-col
         justify-center items-center transition-all 
-        duration-300 overflow-hidden`} >
-
-         {/* close (X) icon */}
- <div onClick={ ()=> setIsOpen(false)} 
-        className='cursor-pointer absolute top-8 right-8'>
-          <XIcon className='w-8 h-8'/>
+        duration-300 overflow-hidden`}
+      >
+        {/* close (X) icon */}
+        <div
+          onClick={() => setIsOpen(false)}
+          className="cursor-pointer absolute top-8 right-8"
+        >
+          <XIcon className="w-8 h-8" />
         </div>
 
-        </motion.ul>
-     
+        {navigation.map((item, index) => {
+          return (
+            <li key={index} className='mb-8'>
+              <Link
+                to={item.href}
+                smooth={true}
+                duration={500}
+                offset={-70}
+                className='text-xl cursor-pointer
+                capitalize'
+              >
+                {item.name}
+              </Link>
+            </li>
+          )
+        })}
+      </motion.ul>
     </nav>
   )
 }
 
-export default NavMobile;
+export default NavMobile
