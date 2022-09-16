d066fd87c8b68b07f571b6a02864c2bea917f159
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-16 17:26:23 +0500

    final commit

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,32 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-// import component
+// import components
 import TestiSlider from './TestiSlider';
 
 const Testimonials = () => {
+
   return (
     <section id='testimonials' className='section bg-secondary'>
-    <div className='container mx-auto'>
-
+      <div className='container mx-auto'>
         <div className='flex flex-col items-center text-center'>
-            <h2 className='section-title 
-                    before:content-testimonials relative 
-                    before:absolute before:opacity-20
-                    before:-top-[2rem] before:-left-64
-                    before:hidden before:lg:block'> 
-                    Reviews
-                    </h2>
-                    <p className="subtitle">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit tempore
-                    earum dolorum quo consequatur rem nihilhic ducimus rerum soluta
-                    neque harum velit molestiae dignissimos distinctio dolorum nisi
-                    labore culpa nihil
-                </p>
+          <h2 className='section-title before:content-testimonials relative before:absolute before:opacity-40 before:-top-[2rem] before:-left-64 before:hidden before:lg:block'>
+            What other people say
+          </h2>
+          <p className='subtitle'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam
+            labore nisium illum cupiditate reiciendis a numquam
+          </p>
         </div>
         <TestiSlider />
-    </div>
-</section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
 export default Testimonials;
