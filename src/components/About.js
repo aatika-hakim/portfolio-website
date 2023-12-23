420f826b706e74f6eb721a5712c24b1bd517e422
Aatika hakim <aatikahakim90@gmail.com>
2023-12-23 11:21:51 +0500

    add info

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ const About = () => {
   return (
     <section id="about" className="section bg-secondary">
       <div className="conainer mx-auto">
-        <div className="flex flex-col xl:flex-row gap-24">
+        <div className="flex flex-col xl:flex-row gap-10">
           <img
-            className="object-cover h-full md:mx-auto 
+            className="object-cover h-[400px] md:mx-auto 
                 w-[566px] lg:ml-20 rounded-2xl"
             src={Image}
             alt=""
@@ -17,7 +17,7 @@ const About = () => {
 
           <div
             className="flex flex-col items-center
-                text-center mr-20 ml-20 lg:items-start lg:text-left"
+                text-center mr-20 lg:items-start lg:text-left"
           >
             <div className="flex flex-col">
               <h2
@@ -32,24 +32,28 @@ const About = () => {
               </h2>
 
               <p
-                className="mb-4
-                text-blue-900"
+                className="mb-2
+                text-accent"
               >
-                MERN Stack Developer
+                Web Developer
               </p>
               <hr className="mb-8 opacity-5" />
-              <p className="mb-8">
-              I am  a MERN  Stack developer with solid understanding of 
-              font-end and back-end technologies, Restful web services and 
-              Data Base designing.               
+              <p style={{ whiteSpace: 'pre-line', marginBottom: '32px' }}>
+                Hello! I'm Aatika Hakim, a passionate web developer with 1 year of experience in creating dynamic websites. I specialize in front-end development and learning backend as well, with expertise in React.js, Next.js, HTML, CSS, JavaScript, and Tailwind CSS.
+                {'\n\n'}
+
+                In my impactful career, I've worked on diverse projects. I'm committed to staying current with web development trends and continuously improving my skills.
+                {'\n\n'}
+
+                I'm passionate about creating exceptional web experiences and am eager to tackle new challenges in web development. My goal is to contribute my expertise to projects that push boundaries and make a positive impact.
               </p>
             </div>
             <button
-              className="btn btn-md bg-blue-900
+              className="btn btn-md bg-accent
               hover:bg-accent-hover md:btn-lg
               transition-all"
             >
-            <a href='#contact'>Contact me</a> 
+              <a href='#contact'>Contact me</a>
             </button>
           </div>
         </div>
